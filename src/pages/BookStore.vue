6cb9c4748431a17d9c2e6dc9d477422f90258ee3
thaksina81 <112239425+thaksina81@users.noreply.github.com>
2022-10-29 11:07:54 +0700

    Add files via upload

---
+++ src/pages/BookStore.vue
@@ -0,0 +1,110 @@
+<template>
+    <Navbar />
+    <section class="about">
+        <h1 class="heading"> book <span>store</span> </h1>
+        <a-table :columns="columns" :data-source="data" :rowKey="(record) => record.id">
+            <template #action="{ record }">
+                <a href="javascript:void(0)" @click="showModal(record.id)">แก้ไข</a>
+                <a-divider type="vertical" />
+                <a href="javascript:void(0)" @click="showConfirmDelete(record.id)">ลบ</a>
+            </template>
+        </a-table>
+        <a-button type="primary" @click="showModal">เพิ่มหนังสือ</a-button>
+    </section>
+
+    <a-modal v-model:visible="modalVisible" title="เพิ่ม/แก้ไข หนังสือ" @ok="handleOk" okText="บันทึก"
+        cancelText="ยกเลิก">
+        <a-row>
+            <a-col :span="6"><label>ชื่อหนังสือ</label></a-col>
+            <a-col :span="18">
+                <a-input v-model:value="book.name" placeholder="ชื่อหนังสือ" />
+            </a-col>
+        </a-row>
+        <br />
+        <a-row>
+            <a-col :span="6"> <label>ราคา</label>
+            </a-col>
+            <a-col :span="18">
+                <a-input-number v-model:value="book.price" :min="0" :max="5000" />
+            </a-col>
+        </a-row>
+    </a-modal>
+</template>
+
+    
+
+<script>
+import Navbar from "../components/Navbar.vue";
+import axios from 'axios'
+import { Modal } from 'ant-design-vue';
+const columns = [
+    {
+        title: "id",
+        dataIndex: "id",
+        key: "id",
+        width: 65,
+    },
+    {
+        title: "ชื่อหนังสือ",
+        dataIndex: "name",
+        key: "name",
+    },
+    {
+        title: "ราคา",
+        dataIndex: "name",
+        key: "price",
+        align: "right"
+    },
+    {
+        title: "Action",
+        slots: { customRender: "action" },
+        key: "id",
+        align: "center",
+        width: 150,
+    }
+];
+export default {
+    components: { Navbar },
+    methods: {
+        async getBooks() {
+            try {
+                let res = await axios.get("http://localhost:3000/books")
+                this.data = res.data
+            } catch (error) { }
+        },
+
+        showModal() {
+            this.book = {
+                id: null,
+                name: null,
+                price: 0,
+            }
+            this.modalVisible = true
+        },
+        async handleOk() { 
+            try {
+                let res = await axios.post("http://localhost:3000/books", this.book) 
+                if (res)
+                    this.modalVisible = false
+            } catch (error) { } finally { this.getBooks() }
+        }
+},
+data() {
+    return {
+        columns: columns,
+            data: [],
+            modalVisible: false,
+            book: {
+                id: null,
+                name: null,
+                price: 0,
+            }
+        }
+    },
+    beforeMount() {
+        this.getBooks()
+    }
+
+}
+</script>
+
